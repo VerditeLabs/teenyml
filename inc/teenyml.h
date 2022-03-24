cd2667717defdab80c846dad3c27ba98c97b8447
VerditeLabs <19700662+VerditeLabs@users.noreply.github.com>
2022-03-24 14:45:53 -0500

    remove macro magic that wasn't portable

--- inc/teenyml.h
+++ inc/teenyml.h
@@ -12,36 +12,20 @@
 
 #define auto __auto_type
 
-//#define OVERLOADABLE __attribute__((overloadable))
-//#define CONST __attribute__((const))
-//#define PURE __attribute__((pure))
-//#define TEENYML_EXPORT OVERLOADABLE static inline
+#define OVERLOADABLE __attribute__((overloadable))
+#define CONST __attribute__((const))
+#define PURE __attribute__((pure))
+#define TEENYML_EXPORT OVERLOADABLE static inline
 
-#define OVERLOADABLE
-#define CONST
-#define PURE
-#define TEENYML_EXPORT
+//#define OVERLOADABLE
+//#define CONST
+//#define PURE
+//#define TEENYML_EXPORT
+
+#define ANON_ARRAY(t, n, ...) (t[n]){__VA_ARGS__}
 
 #define bail(...) do{printf("bailed at %d:%s:%s\n",__LINE__,__FUNCTION__,__FILE__);\
 printf(__VA_ARGS__); exit(1);} while(0)
-
-// Make a FOREACH macro
-#define FE_0(WHAT)
-#define FE_1(WHAT, X) WHAT(X) 
-#define FE_2(WHAT, X, ...) WHAT(X)FE_1(WHAT, __VA_ARGS__)
-#define FE_3(WHAT, X, ...) WHAT(X)FE_2(WHAT, __VA_ARGS__)
-#define FE_4(WHAT, X, ...) WHAT(X)FE_3(WHAT, __VA_ARGS__)
-#define FE_5(WHAT, X, ...) WHAT(X)FE_4(WHAT, __VA_ARGS__)
-//... repeat as needed
-
-#define GET_MACRO(_0,_1,_2,_3,_4,_5,NAME,...) NAME 
-#define FOR_EACH(action,...) \
-  GET_MACRO(_0,__VA_ARGS__,FE_5,FE_4,FE_3,FE_2,FE_1,FE_0)(action,__VA_ARGS__)
-
-
-#define S32IFY(X)   s32 X 
-
-
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -67,7 +51,7 @@ typedef union vecbase_t {
       s32 raw[rank_]; \
       struct {s32 __VA_ARGS__;}; \
     } dims##rank_##_t; \
-    TEENYML_EXPORT CONST dims##rank_##_t dims##rank_## (FOR_EACH(S32IFY, __VA_ARGS__ )){} \
+    TEENYML_EXPORT CONST dims##rank_##_t dims##rank_ (s32 dims[static rank_]){ return (dims##rank_##_t){.raw = dims};} \
     TEENYML_EXPORT CONST s32 rank(dims##rank_##_t dims){ return rank_; } \
     TEENYML_EXPORT CONST s32 idx(dims##rank_##_t dims, dims##rank_##_t idx){ s32 i = 0, sz = 1; for(s32 _ = rank_ - 1; _ >= 0; _--) {i += idx.raw[_]*sz; sz*=dims.raw[_];} return i;}
 
--- src/testmain.c
+++ src/testmain.c
@@ -5,13 +5,13 @@
 
 
 static MunitResult test_teenyml_dims(const MunitParameter params[], void* user_data) {
-	dims3_t d = {}
+	dims3_t d = dims3(ANON_ARRAY(s32,3,3,6,9));
 
 	return MUNIT_OK;
 }
 
 static MunitTest teenyml_tests[] = {
-    { (char*)"/teenyml", test_teenyml, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+    { (char*)"/teenyml", test_teenyml_dims, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
     { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
 
